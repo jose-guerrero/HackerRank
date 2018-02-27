1347faceeab195c733bf69c3a9408b17fc9f25f2
jose-guerrero <jose.guerrerog@outlook.com>
2018-02-27 23:02:48 +1100

    c-sharp

---
+++ Plus_Minus/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void plusMinus(int[] arr) {
+        
+        int num1 = 0,num2 = 0,num3 = 0; 
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i]>0) num1++;
+            else {
+                if (arr[i]<0) num2++;
+                else num3++;
+            }
+             
+        }
+        Console.WriteLine((num1*1.0/arr.Length).ToString("N6"));
+        Console.WriteLine((num2*1.0/arr.Length).ToString("N6"));
+        Console.WriteLine((num3*1.0/arr.Length).ToString("N6"));
+    }
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp,Int32.Parse);
+        plusMinus(arr);
+    }
+}
+
