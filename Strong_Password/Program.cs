09f98c961c12fc31744de66148d39b063d67b3e9
jose-guerrero <jose.guerrerog@outlook.com>
2018-02-27 22:50:29 +1100

    c-sharp strings

---
+++ Strong_Password/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static int minimumNumber(int n, string password) {
+        int cont = 0;
+
+        foreach (char obj in "!@#$%^&*()-+")
+            if (password.Contains(obj.ToString()))
+            {
+                cont++;
+                break;
+            }
+
+        if (password.Any(char.IsDigit))
+        {
+            cont++;
+        }
+        if (password.Any(char.IsUpper))
+        {
+            cont++;
+        }
+        
+        if (password.Any(char.IsLower))
+        {
+            cont++;
+        }
+
+        return 4 - cont + Math.Max(6-(n+4-cont),0);
+    }
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string password = Console.ReadLine();
+        int answer = minimumNumber(n, password);
+        Console.WriteLine(answer);
+    }
+}
+
