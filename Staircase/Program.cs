9c53ef73df45dd83f5dbe9e178d2120232365153
jose-guerrero <jose.guerrerog@outlook.com>
2018-03-05 16:30:29 +1100

    c-sharp

---
+++ Staircase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void staircase(int n) {
+        for (int i=0;i<n;i++)
+        {
+            string s = "";
+            for (int j=0;j<=n-i-2;j++){
+                s+=" ";
+            }
+            for (int j=n-i-1;j<n;j++){
+                s+="#";
+            }
+            Console.WriteLine(s);
+        }
+    }
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        staircase(n);
+    }
+}
