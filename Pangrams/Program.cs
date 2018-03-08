d3ffe6ff3a08ee4d4d06ae9c15d0299f8b7a024e
jose-guerrero <jose.guerrerog@outlook.com>
2018-03-08 23:25:38 +1100

    strings

---
+++ Pangrams/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solution {
+class Solution {
+    static void Main(string[] args) {
+
+        string s = Console.ReadLine();
+        bool[] taken = new bool[26];
+        int res = 0;
+
+
+        for (int i=0;i<s.Length;i++)
+        {
+            if (Char.IsLower(s[i]) && taken[s[i]-'a']!=true)
+            {
+                taken[s[i]-'a'] = true;
+                res++;
+            }
+            else
+            {
+                if (Char.IsUpper(s[i]) && taken[s[i]-'A']!=true)
+                {
+                    taken[s[i]-'A'] = true;
+                    res++;
+                }
+            }
+        }
+
+        if (res == 26)
+        {
+            Console.WriteLine("pangram");
+        }
+        else
+        {
+            Console.WriteLine("not pangram");
+        }
+    
+    }
+}
+}
+
