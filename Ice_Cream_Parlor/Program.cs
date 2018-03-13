cc21efd75257fea0076727cd9fb4c9de1921db8e
jose-guerrero <jose.guerrerog@outlook.com>
2018-03-13 17:21:18 +1100

    search

---
+++ Ice_Cream_Parlor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+class Solution {
+
+    static int[] icecreamParlor(int m, int[] arr) {
+        int[]res = new int[2];
+        
+        for (int i=0;i<arr.Length;i++)
+        {
+            for (int j=i+1;j<arr.Length;j++)
+            {
+                int p = m - arr[i];
+                if (p == arr[j])
+                {
+                    res[0] = i+1;
+                    res[1] = j+1;
+                   return res;
+                }
+            }
+        }
+        return res;
+    }
+
+    static void Main(String[] args) {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for(int a0 = 0; a0 < t; a0++){
+            int m = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp,Int32.Parse);
+            int[] result = icecreamParlor(m, arr);
+            Console.WriteLine(String.Join(" ", result));
+
+
+        }
+    }
+}
+
