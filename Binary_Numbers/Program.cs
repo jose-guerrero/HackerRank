2f892330c2d92dae6c850f63664de8be6e6d9262
jose-guerrero <jose.guerrerog@outlook.com>
2018-04-17 10:45:58 +1000

    warmup

---
+++ Binary_Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Binary_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = 0,ans = 0;
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            while (n>0)
+            {
+                int m = n%2;
+                n = n/2;
+
+                if (m==1)
+                {
+                    temp++;
+                }
+                else
+                {
+                    temp = 0;
+                    ans = Math.Max(ans,temp);
+                }
+            }
+            ans = Math.Max(ans,temp);
+            Console.WriteLine(ans);
+        }
+    }
+}
