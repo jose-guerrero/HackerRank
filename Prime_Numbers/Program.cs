29d2854a61191cb0d01b1cbe3ba09793b009c71d
jose-guerrero <jose.guerrerog@outlook.com>
2018-05-01 14:20:14 +1000

    warmup

---
+++ Prime_Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) {
+        
+        int t = Convert.ToInt32(Console.ReadLine());
+        
+        for (int i = 1;i<=t;i++){
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            int m = (int)Math.Sqrt(num);
+            
+            bool res = false;
+   
+            for (int j=2;j<=(m+1);j++)
+            {
+                if (num%j == 0){
+                    res = true;
+                    break;
+                }
+            }
+            if (res == true || num==1) Console.WriteLine("Not prime");
+            else Console.WriteLine("Prime");
+        }
+    }
+}
